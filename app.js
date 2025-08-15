241788d2a4205ed0f22afd46e63665fd87a7d1ed
Aristides Alejandro Rodriguez <rodriguezaristidesalejandro@gmail.com>
2025-08-15 20:30:03 -0300

    Pequeña corrección

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-function ingresarAmigo()
+function ingresarAmigo() {
+
+    //Agregar nombre obtenido a travez del imput
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    // Validar que no se ingrese un input vacio
+    if (amigo === ""){
+        alert("Por favor ingrese un nombre valido");
+        return
+   }
+
+}
